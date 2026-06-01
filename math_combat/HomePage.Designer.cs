bc7753b25daf70712147f959ed8fa46f47f963aa
molingyuan <linarielfour@gmail.com>
2026-06-02 00:34:39 +0800

    add new pages, fix no sln problem
    把sln加回去不然好難開

---
+++ math_combat/HomePage.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace math_combat
+{
+    partial class HomePage
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.title = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox1.BackColor = System.Drawing.Color.Silver;
+            this.pictureBox1.Location = new System.Drawing.Point(21, 21);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Padding = new System.Windows.Forms.Padding(10);
+            this.pictureBox1.Size = new System.Drawing.Size(900, 450);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // title
+            // 
+            this.title.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.title.AutoSize = true;
+            this.title.BackColor = System.Drawing.Color.Silver;
+            this.title.Font = new System.Drawing.Font("幼圆", 50F, System.Drawing.FontStyle.Bold);
+            this.title.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.title.Location = new System.Drawing.Point(207, 70);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(545, 84);
+            this.title.TabIndex = 1;
+            this.title.Text = "數字運算對戰";
+            this.title.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.AutoSize = true;
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(156)))), ((int)(((byte)(124)))));
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(156)))), ((int)(((byte)(124)))));
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(108)))), ((int)(((byte)(38)))), ((int)(((byte)(38)))));
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(156)))), ((int)(((byte)(124)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("幼圆", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Location = new System.Drawing.Point(341, 190);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(280, 94);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "私人對戰";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Paint += new System.Windows.Forms.PaintEventHandler(this.button1_Paint);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Silver;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("幼圆", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.Location = new System.Drawing.Point(390, 300);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(180, 60);
+            this.button2.TabIndex = 3;
+            this.button2.Text = ">  遊戲規則";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Silver;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("幼圆", 15F);
+            this.button3.Location = new System.Drawing.Point(390, 366);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(180, 60);
+            this.button3.TabIndex = 4;
+            this.button3.Text = ">  音效設定";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(646, 228);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("幼圆", 12F);
+            this.label1.Location = new System.Drawing.Point(53, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // HomePage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Maroon;
+            this.ClientSize = new System.Drawing.Size(942, 493);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.title);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximumSize = new System.Drawing.Size(960, 540);
+            this.MinimumSize = new System.Drawing.Size(960, 540);
+            this.Name = "HomePage";
+            this.Text = "math_combat";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ math_combat/HomePage.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace math_combat
+{
+    public partial class HomePage : Form
+    {
+        // 宣告一個私有的 PrivateFontCollection 來存放字體
+        private PrivateFontCollection privateFonts = new PrivateFontCollection();
+        private Font customFont;
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadCustomFont();
+
+            // 套用字體給指定的控制項
+            button1.Font = new Font(privateFonts.Families[0], 20, FontStyle.Regular);
+            label1.Font = new Font(privateFonts.Families[0], 15, FontStyle.Regular);
+        }
+
+        //載入字體
+        private void LoadCustomFont()
+        {
+            // 字體檔案放在輸出的資料夾路徑
+            string fontPath = System.IO.Path.Combine(Application.StartupPath, "jf-openhuninn-2.1.ttf");
+
+            try
+            {
+                // 將字體檔案載入到 privateFonts 集合中
+                privateFonts.AddFontFile(fontPath);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("字體載入失敗: " + ex.Message);
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.FlatStyle = FlatStyle.Flat;
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Paint(object sender, PaintEventArgs e)
+        {
+            Button btn = (Button)sender;
+
+            // 設定圓角半徑（數字越大越圓）
+            int borderRadius = 5;
+
+            // 啟用抗鋸齒，讓圓角邊緣看起來更平滑
+            e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+            // 建立一個圓角矩形的路徑
+            System.Drawing.Drawing2D.GraphicsPath path = new System.Drawing.Drawing2D.GraphicsPath();
+            path.StartFigure();
+            path.AddArc(0, 0, borderRadius, borderRadius, 180, 90); // 左上角
+            path.AddArc(btn.Width - borderRadius, 0, borderRadius, borderRadius, 270, 90); // 右上角
+            path.AddArc(btn.Width - borderRadius, btn.Height - borderRadius, borderRadius, borderRadius, 0, 90); // 右下角
+            path.AddArc(0, btn.Height - borderRadius, borderRadius, borderRadius, 90, 90); // 左下角
+            path.CloseFigure();
+
+            // 關鍵：將按鈕的形狀裁剪成我們設定的圓角路徑
+            btn.Region = new Region(path);
+
+        }
+
+    }
+}
